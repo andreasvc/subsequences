26dd8225df199da0c5948b87b8a745a7e67ddfa8
Andreas van Cranenburgh <andreas@unstable.nl>
2012-12-10 17:32:06 +0100

    batch mode, proper command line parsing

--- lcs.py
+++ lcs.py
@@ -1,5 +1,6 @@
 """ Longest common subsequence for two texts. """
 import sys, os
+from getopt import gnu_getopt, GetoptError
 from _lcs import getsequences
 
 USAGE = """usage: %s text1 text2 [--all] [--debug]
@@ -8,26 +9,47 @@ one sentence per line, words space separated.
 Output will be a list of the longest common subsequences found
 in each sentence pair, preceded by its occurrence frequency and a tab.
 
-	--all	enable collection of all longest common subsequences.
-	--debug	dump chart with lengths of common subsequences for two sequences.
+    --all   enable collection of all longest common subsequences.
+    --debug dump charts with lengths of common subsequences.
+    --batch dir
+            compare text1 to an arbitrary number of other given texts, and
+            write the results to dir/A_B for file A compared to B.
+    --quiet ignored
 """ % sys.argv[0]
 
 def main():
 	# command line arguments
-	if len(sys.argv) < 3:
-		print USAGE
-	elif not os.path.exists(sys.argv[1]):
-		print "file not found:", sys.argv[1]
-	elif not os.path.exists(sys.argv[2]):
-		print "file not found:", sys.argv[2]
-	else:
-		# find subsequences
-		results = getsequences(sys.argv[1], sys.argv[2],
-				getall="--all" in sys.argv, debug="--debug" in sys.argv)
-
+	flags = ("quiet", "debug", "all")
+	options = ("batch=", )
+	try:
+		opts, args = gnu_getopt(sys.argv[1:], "", flags + options)
+	except GetoptError as err:
+		print "error: %r\n%s" % (err, USAGE)
+		exit(2)
+	opts = dict(opts)
+	for filename in args:
+		assert os.path.exists(filename), "file not found: %r" % filename
+	if '--batch' not in opts:
+		assert len(args) == 2, USAGE
 		# dump subsequences to stdout
-		for subseq, count in results.iteritems():
-			print "%d\t%s" % (count, " ".join(subseq))
+		outfile = sys.stdout
+	else:
+		assert len(args) >= 2, USAGE
+	filename1 = args[0]
+	for filename2 in args[1:]:
+		# find subsequences
+		results = getsequences(filename1, filename2,
+			getall="--all" in opts)
+
+		if '--batch' in opts:
+			# dump subsequences to file in given directory
+			outfile = open("%s/%s_%s" % (opts['--batch'],
+				os.path.basename(filename1),
+				os.path.basename(filename2)), "w")
+		outfile.writelines("%s\t%d\n" % (" ".join(subseq), count)
+				for subseq, count in results.iteritems())
+		if '--batch' in opts:
+			outfile.close()
 
 if __name__ == '__main__':
 	main()
